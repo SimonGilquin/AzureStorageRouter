7a30968c59dbb3f42c4a3e7c619e1fc09e82c479
Simon Gilquin <simon@gilquin.com>
2014-12-14 22:40:37 +0100

    Added simple test client

---
+++ AzureStorageRouter.Client/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureStorageRouter.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new WebClient();
+            var filePath = Path.Combine(Path.GetTempPath(), "TestFile.txt");
+            File.WriteAllText(filePath, "This is a test file for the Azure Storage Router");
+            var result = client.UploadFile("http://localhost:42553/api/documents", filePath);
+            var resultData = Encoding.UTF8.GetString(result);
+            Console.WriteLine(resultData);
+        }
+    }
+}
